f8e518706c2b08285a4a2f574bf29f36919845b7
Steffen Reith <Steffen.Reith@hs-rm.de>
2024-03-13 14:55:54 +0100

    Initial checkin

---
+++ src/test/scala/ArgsConfig.scala
@@ -0,0 +1,15 @@
+/*
+ * Author: Steffen Reith (steffen.reith@hs-rm.de)
+ *
+ * Creation Date:  Wed Mar 13 14:54:42 CET 2024
+ * Module Name:    ArgConfig - Holds all information after parsing command line parameter s
+ * Project Name:   PipelinedMultiply - A demo for SpinalHDL's pipeline framework
+ *
+ */
+
+// Used by scopt to store information
+case class ArgsConfig(useDevicePrefix    : Boolean,
+                      createWaveFile     : Boolean,
+                      serialDevicePrefix : String,
+                      serialDeviceName   : String,
+                      simSeed            : Option[Int])
